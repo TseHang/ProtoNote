160561f6b493a187fc5b5a540ad266c607469b1f
TseHang <a54383813@gmail.com>
2020-10-18 10:16:58 +0800

    chore: set up styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,23 @@
-import '@/styles/global.css';
-
 import { AppProps } from 'next/app';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
+import GlobalStyle from '@/styles/global.ts';
+import { theme } from '@/styles/theme';
 import { useApollo } from '@/utils/apolloClient';
 import { ApolloProvider } from '@apollo/client';
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
-  
+
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <GlobalStyle />
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link';
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  color: ${p => p.theme.colors.main};
+  text-decoration: underline;
+`;
 
 const App = () => {
-  return <div>
-    123
-    <Link href="/test">go test</Link>
+  return (
+    <div>
+      123
+      <Link href="/test">
+        <StyledLink>go test</StyledLink>
+      </Link>
     </div>
-}
+  );
+};
 
 export default App;
